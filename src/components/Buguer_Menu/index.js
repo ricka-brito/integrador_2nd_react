52e7afe10e2fad7807a9048972f2fac8c1cbae1f
rick <rickabrito@gmail.com>
2023-10-19 17:00:10 -0300

    a

--- src/components/Buguer_Menu/index.js
+++ src/components/Buguer_Menu/index.js
@@ -5,10 +5,10 @@ import './burger.css';
 
 function BurguerMenu() {
   return (
-    <div className='menu'>
-        <FontAwesomeIcon color='#fff' size='3x' icon={faBars} />
+    <a href="" className='menu'>
+        <FontAwesomeIcon color='#fff' size='2x' icon={faBars} />
         <h1 className='textMenu'>MENU</h1>
-    </div>
+    </a>
   )
 }
 
--- src/components/Second_Section/index.js
+++ src/components/Second_Section/index.js
@@ -9,14 +9,35 @@ export default function SecondSection() {
     const [app, setApp] = useState("noAniApp")
     const [cell, setCell] = useState("noAniCell")
 
+    const [part1, setPart1] = useState("")
+    const [part2, setPart2] = useState("")
+    const [part3, setPart3] = useState("")
+    const [part4, setPart4] = useState("")
+
     const listenScrollEvent = (event) => {
         if (window.scrollY > 600) {
         return (setThe("aniThe"), setCell('aniCell'), setApp("aniApp"))
         } 
     }
 
+    const listenScrollEventText = (event) => {
+        if (window.scrollY < 1600) {
+            return (setPart1('high'), setPart2(''), setPart3(''), setPart4(''))
+        } 
+        if (window.scrollY < 1750) {
+            return (setPart1(''), setPart2('high'), setPart3(''), setPart4(''))
+        } 
+        if (window.scrollY < 1900) {
+            return (setPart1(''), setPart2(''), setPart3('high'), setPart4(''))
+        }
+        if (window.scrollY < 2050) {
+            return (setPart1(''), setPart2(''), setPart3(''), setPart4('high'))
+        }  
+    }
+
     useEffect(() => {
         window.addEventListener('scroll', listenScrollEvent);
+        window.addEventListener('scroll', listenScrollEventText);
 
         return () =>
         window.removeEventListener('scroll', listenScrollEvent);
@@ -24,14 +45,25 @@ export default function SecondSection() {
 
 
     return (
-    <div className="second-section">
-        <h1 id="the" className={`text-app ${the}`}>
-            The
-        </h1>
-        <img className={`phone ${cell}`} src={SmartPhone}/>
-        <h1 id="app" className={`text-app ${app}`}>
-            App
-        </h1>
-    </div>
+        <>
+            <div className="second-section">
+                <h1 id="the" className={`text-app ${the}`}>
+                    The
+                </h1>
+                <img className={`phone ${cell}`} src={SmartPhone}/>
+                <h1 id="app" className={`text-app ${app}`}>
+                    App
+                </h1>
+            </div>
+            <div className="desc">
+                <h1 className="desc_second_div">
+                    <span className={`desc_second ${part1}`}>Download Swift.</span><br/>
+                    <span className={`desc_second ${part2}`}>Your purchases, anywhere, anytime.</span>
+                    <span className={`desc_second ${part3}`}> Easily transfer money, for free. </span><br/>  
+                    <span className={`desc_second ${part4}`}>Bank from anywhere in the world.</span>
+                </h1>
+            </div>
+        </>
+   
     )
 }
