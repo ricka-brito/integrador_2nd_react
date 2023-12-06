2b277ad71d250734f363f3eb12cd8dc685617203
henriquebrito <rickabrito@gmail.com>
2023-12-06 04:35:43 -0300

    validation (still need the cpf api)

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -7,6 +7,8 @@ import Button from "../../components/Button";
 import { useNavigate } from 'react-router-dom';
 import { GlobalContext } from '../../util/Context';
 import { useContext } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function SideBar({sidebaractive, setBar}) {
     const [cpf, setCpf] = useState()
@@ -22,6 +24,50 @@ export default function SideBar({sidebaractive, setBar}) {
         //realizar a formatação...
           setCpf(cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4"));
       }
+
+      function validateCPF(cpf) {
+        cpf = cpf.replace(/[^\d]+/g, ''); // Remove caracteres não numéricos
+      
+        if (cpf.length !== 11 || /^(.)\1+$/.test(cpf)) {
+          return false; // CPF deve ter 11 dígitos e não pode ser uma sequência repetida
+        }
+      
+        // Calcula os dígitos verificadores
+        let sum = 0;
+        let remainder;
+      
+        for (let i = 1; i <= 9; i++) {
+          sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        }
+      
+        remainder = (sum * 10) % 11;
+      
+        if (remainder === 10 || remainder === 11) {
+          remainder = 0;
+        }
+      
+        if (remainder !== parseInt(cpf.substring(9, 10))) {
+          return false; // Primeiro dígito verificador inválido
+        }
+      
+        sum = 0;
+        for (let i = 1; i <= 10; i++) {
+          sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+        }
+      
+        remainder = (sum * 10) % 11;
+      
+        if (remainder === 10 || remainder === 11) {
+          remainder = 0;
+        }
+      
+        if (remainder !== parseInt(cpf.substring(10, 11))) {
+          return false; // Segundo dígito verificador inválido
+        }
+      
+        return true; // CPF válido
+      }
+
     return (
         <div className={sidebaractive ? 'sidebar-all' : ''} style={{width:sidebaractive ? '100%' : '0', height: sidebaractive ? "100%" : "0"}}>
             <div className={sidebaractive === true ? 'sidebar' : sidebaractive === false ? 'sidebarclose': ''}>
@@ -35,10 +81,24 @@ export default function SideBar({sidebaractive, setBar}) {
                     <Input placeholder="CPF" id="cpf" formater={formataCPF} value={cpf} maxlength={"14"}/>
                     <Button buttonText="continue"
                      onClick={() => {
-                        setState({cpf: cpf})
-                        navigate('/signup')
+                        if(validateCPF(cpf)){
+                            setState({cpf: cpf})
+                            navigate('/signup')
+                        }
+                        else{
+                            toast.error('Invalid CPF', {
+                                position: "top-right",
+                                autoClose: 5000,
+                                hideProgressBar: false,
+                                closeOnClick: true,
+                                pauseOnHover: true,
+                                draggable: true,
+                                progress: undefined,
+                                theme: "dark",
+                                });
+                        }
                         }}/>
-                    
+                    <ToastContainer theme="dark" />
                     <div className="separator">
                         <div className="linea"/>
                         <p>OR</p>
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -7,8 +7,12 @@ import FilesDragAndDrop from '../../components/DragAndDrop';
 import { GlobalContext } from '../../util/Context';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
-
+import QRcode from "../../assets/images/qrcode.png";
+import { IoMdClose } from "react-icons/io";
+import { Link } from "react-router-dom";
 import API_URL from '../../util/Constants'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Signup() {
   
@@ -32,6 +36,7 @@ export default function Signup() {
   const [city, setCity] = useState();
   const [number, setNumber] = useState();
   const [state, setState] = useState();
+  const [sucessefulMessage, setSucessefulMessage] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -43,6 +48,40 @@ export default function Signup() {
     }
   }, [])
 
+  const handlePhoneNumberChange = (phone) => {
+    let formattedNumber = phone.replace(/\D/g, ''); // Remove caracteres não numéricos
+
+    if (formattedNumber.length <= 2) {
+      formattedNumber = formattedNumber.replace(/(\d{1})/, "($1")
+    }
+    if (formattedNumber.length >= 2 && formattedNumber.length < 7){
+      formattedNumber = formattedNumber.replace(/(\d{2})(\d)/, "($1) $2")
+    }
+    if (formattedNumber.length == 7){
+      formattedNumber = formattedNumber.replace(/(\d{2})(\d{4})/, "($1) $2-")
+    }
+    if (formattedNumber.length > 6 && formattedNumber.length < 10){
+      formattedNumber = formattedNumber.replace(/(\d{2})(\d{4})(\d)/, "($1) $2-$3")
+    }  
+    if (formattedNumber.length >= 11){
+      console.log("teste")
+      formattedNumber = formattedNumber.replace(/(\d{2})(\d{5})(\d)/, "($1) $2-$3")
+    }
+
+    if (formattedNumber.length == 10){
+      formattedNumber = formattedNumber.replace(/(\d{2})(\d{4})(\d)/, "($1) $2-$3")
+
+    }
+
+
+    setTelefone(formattedNumber)
+  }
+
+  const formatCep = (cep) => {
+    let formattedCep = cep.replace(/\D/g, ''); // Remove caracteres não numéricos
+    setCep(formattedCep)
+  }
+
   const formatCur = (num) => {
     let value = num
     value = value.replace(/\D/g, "")
@@ -53,17 +92,33 @@ export default function Signup() {
 
   return (
     <div className="body-signup">
+      <Link to="/"><IoMdClose size={50} className='close-sign'/></Link>
       <div className="formdata-signup">
         {firstStep == true && <>
           <img className='logo' src={Icon}/>
           <h3 className='title-sign'>Finish your signup</h3>
           <Input className="input-nome" placeholder="Nickname" id="nome" formater={setNome} value={nome}/>
           <Input type='email'className="input-email" placeholder="Email" id="email" formater={setEmail} value={email}/>
-          <Input type='number' className="input-email" placeholder="Phone number" id="telefone" formater={setTelefone} value={telefone} maxlength={"14"}/>
+          <Input className="input-email" placeholder="Phone number" id="telefone" formater={handlePhoneNumberChange} value={telefone} maxlength={"15"}/>
           <Button buttonText="continue" className="botao" onClick={() => {
-            setFirstStep(false)
-            setSecondStep(true)
+            if(telefone.replace(/\D/g, '').length >= 10){
+              setFirstStep(false)
+              setSecondStep(true)
+            }
+            else{
+              toast.error('Invalid PHONE', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
+            }
           }}/>
+          <ToastContainer theme="dark" />
           </>
         }
 
@@ -71,7 +126,7 @@ export default function Signup() {
           secondStep == true && <>
          <img className='logo' src={Icon}/>
          <h3 className='title-sign'>Now we need data of your adress</h3>
-         <Input type='number' className="input-cep" placeholder="CEP" id="cep" formater={setCep} value={cep} maxlength={"8"} autofocus={true} onBlur={() => {
+         <Input className="input-cep" placeholder="CEP" id="cep" formater={formatCep} value={cep} maxlength={"8"} autofocus={true} onBlur={() => {
 
           fetch(`https://viacep.com.br/ws/${cep}/json/`).then(response => response.json()).then(data => {
             setStreet(data.logradouro)
@@ -89,11 +144,26 @@ export default function Signup() {
          <Input className="input-disabled" placeholder="State" id="state" formater={setState} value={state} readonly={true}/>      
          <Button buttonText="continue" className="botao"
           onClick={() => {
-            setSecondStep(false)
-            setThirdStep(true)
+            if(number){
+              setSecondStep(false)
+              setThirdStep(true)
+            }
+            else{
+              toast.error('Missing house number', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
+            }
+
           }}
           />
-          
+          <ToastContainer theme="dark" />
           </>
 
         }
@@ -105,10 +175,25 @@ export default function Signup() {
           <p className='letrinhas'>We will not share this data and, and it´s will be used to make a deep credit analylis</p>
           <Button buttonText="continue" className="botao"
           onClick={() => {
-            setThirdStep(false)
-            setForthStep(true)
+            if (Number(profit.replaceAll(".", "").replaceAll(",", ".").replaceAll("R$", "")) > 0.01){
+              setThirdStep(false)
+              setForthStep(true)
+            }
+            else {
+              toast.error('The declared profit must be greater than R$0,01', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
+            }
           }}
           />
+          <ToastContainer theme="dark" />
           </>
         }
           
@@ -119,10 +204,25 @@ export default function Signup() {
           <FilesDragAndDrop className="input-files" fileState={setFile}/>
           <Button buttonText="continue" className="botao"
           onClick={() => {
+            if (file){
             setForthStep(false)
             setEndStep(true)
+            }
+            else {
+              toast.error('You must attach a picture', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
+            }
           }}
           />
+          <ToastContainer theme="dark" />
 
           </>
         }
@@ -136,84 +236,108 @@ export default function Signup() {
           <Input type="password" className="input-password" placeholder="Confirm your password (6 digits)" id="confirmPassword" formater={setConfirmPassword} value={confirmPassword} maxlength={"6"}/>
          <Button buttonText="submit" className="botao"
          onClick={() => {
-          setEndStep(false)
-
-          const bodya = {
-            "email": email,
-            "password": password,
-            "first_name": nome,
-            "cpf": cpf.replaceAll(".", "").replaceAll("-", ""),
-            "phone_number": telefone,
-            "declared_salary": Number(profit.replaceAll(".", "").replaceAll(",", ".").replaceAll("R$", "")),
-            "addresses": [
-              {
-                "street": street,
-                "neighborhood": neighborhood,
-                "house_number": number,
-                "city": city,
-                "cep": cep.replaceAll('-', ''),
-                "uf": state
-              }
-            ]
-          }
-          
-
-          fetch(`${API_URL}/api/v1/user/create/`, {
+          if (password == confirmPassword){
+            setEndStep(false)
+            setSucessefulMessage(true)
+  
+            const bodya = {
+              "email": email,
+              "password": password,
+              "first_name": nome,
+              "cpf": cpf.replaceAll(".", "").replaceAll("-", ""),
+              "phone_number": telefone,
+              "declared_salary": Number(profit.replaceAll(".", "").replaceAll(",", ".").replaceAll("R$", "")),
+              "addresses": [
+                {
+                  "street": street,
+                  "neighborhood": neighborhood,
+                  "house_number": number,
+                  "city": city,
+                  "cep": cep.replaceAll('-', ''),
+                  "uf": state
+                }
+              ]
+            }
+            
+  
+            fetch(`${API_URL}/api/v1/user/create/`, {
+              method: "POST",
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(bodya),
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        fetch(`${API_URL}/api/token/`, {
             method: "POST",
             headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify(bodya),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      fetch(`${API_URL}/api/token/`, {
-          method: "POST",
-          headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({"cpf": cpf.replaceAll(".", "").replaceAll("-", ""), "password": password})
-      })
-      .then(response => response.json()).then(async data => {
-        const formData = new FormData();
-        formData.append('image', file[0], 'image.jpg'); // Append the file directly
-
-        // Upload the image
-        fetch(`${API_URL}/api/v1/user/me/`, {
-          'method': 'PATCH', 
-          'headers': {
-            'Authorization': `Bearer ${data.access}`,
-          },
-          'body': formData,
+            body: JSON.stringify({"cpf": cpf.replaceAll(".", "").replaceAll("-", ""), "password": password})
         })
-          .then((response) => {
-            return response.json();
+        .then(response => response.json()).then(async data => {
+          const formData = new FormData();
+          formData.append('image', file[0], 'image.jpg'); // Append the file directly
+  
+          // Upload the image
+          fetch(`${API_URL}/api/v1/user/me/`, {
+            'method': 'PATCH', 
+            'headers': {
+              'Authorization': `Bearer ${data.access}`,
+            },
+            'body': formData,
           })
-          .then((data) => {
-            // Additional handling of the response if needed
-          })
-          .catch((error) => {
-            console.error('Error uploading image:', error.message);
-          });
-
-        
-        // } catch (error) {
-        //   console.error('Error converting URI to Blob:', error);
-        //   return null;
-        // }
-        
+            .then((response) => {
+              return response.json();
+            })
+            .then((data) => {
+              // Additional handling of the response if needed
+            })
+            .catch((error) => {
+              console.error('Error uploading image:', error.message);
+            });
+  
+          
+          // } catch (error) {
+          //   console.error('Error converting URI to Blob:', error);
+          //   return null;
+          // }
+          
+        })
       })
-    })
-    .catch((error) => {
-      console.error('Error uploading image:', error);
-    });
+      .catch((error) => {
+        console.error('Error uploading image:', error);
+      });
+          }
+          else {
+            toast.error('Passwords must be equal', {
+              position: "top-right",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
+              });
+          }
    
 
          }}/>
           </>
+
+          
         }
+        {
+            sucessefulMessage && <>
+            <img className='logo' src={Icon}/>
+            <h3 className='title-sign'>Sucesseful! Welcome to SWIFT! <br/> Download the app to continue.</h3>
+            <img className="qrcode-sucessuful" src={QRcode}/>
+            </>
+          }
       </div>
     </div>
   )
